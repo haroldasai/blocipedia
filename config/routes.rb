3e2fdb4c153392300e9c45aa9a174b802c60587f
Harold Asai <koichiroasai@gmail.com>
2017-06-06 09:34:47 -0700

    server port modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
+#require_relative '../lib/route_scoper'
+
 Rails.application.routes.draw do
+  #scope RouteScoper.root do
+  #get 'welcome/index'
+
+  #get 'welcome/about'
+
+  root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -53,4 +61,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+# end
 end
